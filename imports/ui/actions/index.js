ffb32eded869fe7e705f8e02c66946c42b406355
Servio Mora <servio.mora19@gmail.com>
2018-03-02 14:53:29 -0500

    slight updates and comments

--- imports/ui/actions/index.js
+++ imports/ui/actions/index.js
@@ -1,21 +1,10 @@
 import axios from 'axios';
 import { GET_DATA } from './types';
 
-// export const dataFetch = () => {
-//   return (dispatch) = () => {
-//     axios.get('https://jsonplaceholder.typicode.com/users')
-//       .then(response => {
-//         const data = response.data;
-//         dispatch({ type: GET_DATA, payload: data });
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
-// };
-
 export const dataFetch = () => {
   const request = axios.get('https://jsonplaceholder.typicode.com/users');
+
+  console.log('Request: ' + request);
 
   return {
     type: GET_DATA,
--- imports/ui/reducers/dataReducer.js
+++ imports/ui/reducers/dataReducer.js
@@ -1,10 +1,12 @@
 import { GET_DATA } from '../actions/types';
 
 export default (state = [], action) => {
+  console.log('Action received', action);
+
   switch (action.type) {
     case GET_DATA:
-      return [ ...state, ...action.payload.data ];
+      return [ ...action.payload.data, ...state ] //new data first
   }
 
   return state;
-};
+}
